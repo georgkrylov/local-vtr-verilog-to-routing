8c1b2f0589402f22de74284d04f7442e251f3de8
Georgiy Krylov <georgiy.krylov@unb.ca>
2020-01-31 16:58:08 -0400

    New commit for hard and soft logic mixing
    This commit has all required changes to enable working on the
    optimization, however there is no history and unnecessary files on it

---
+++ ODIN_II/SRC/include/HardSoftLogicMixer.hpp
@@ -0,0 +1,106 @@
+/*
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef HARD_SOFT_LOGIC_MIXER_HPP
+#define HARD_SOFT_LOGIC_MIXER_HPP
+
+#include "physical_types.h"
+#include "odin_types.h"
+#include <utility>      // std::pair, std::make_pair
+#include <map>
+#include <vector>
+#include "OdinGridAnalyzer.hpp"
+#include "multipliers.h"
+#include "netlist_analyzer.h"
+
+class HardSoftLogicMixer{
+public:
+    HardSoftLogicMixer(t_arch& arch,const config_t configuration,std::vector<t_physical_tile_type> tileTypes);
+    /*----------------------------------------------------------------------
+    * Function: calculateGridSizes 
+    *   For all the layouts represented in the architecture file, populates
+    *   the _grid_layout_sizes map with corresponding estimated sizes
+    *---------------------------------------------------------------------*/
+    void calculateAllGridSizes();
+
+    /* Returns whether the hard blocks and soft logic implementation 
+     * of multipliers should be mixed in the process of synthesis
+     */
+    bool softenable(mix_hard_blocks type);
+    /*----------------------------------------------------------------------
+     * Function: map_deferred_blocks 
+     * For all  noted nodes, that were noted as candidates to be implemented 
+     * on the hard blocks, launches corresponding procedure of chosing the
+     * corresponding blocks
+     * Parameters: netlist_t *
+     * returns:
+     *---------------------------------------------------------------------*/
+    void map_deferred_blocks( netlist_t * netlist);
+
+    /*----------------------------------------------------------------------
+     * Function: note_candidate_node
+     * Calculates number of available hard blocks by issuing a call,
+     * traverses the netlist and statistics to figure out
+     * which operation should be implemented on the hard block
+     * Parameters: 
+     *      node_t * : pointer to candidate node
+     *      mix_hard_blocks : type of the hard block to optimize
+     * returns:
+     *---------------------------------------------------------------------*/
+    void note_candidate_node( nnode_t * node, mix_hard_blocks type);
+
+    
+private:
+    /* This function parses the configuration bitmask and sets
+     * the corresponding boolean variables in the _enabledOptimizationsArray.
+     */
+    void parseAndSetOptimizationParameters(const config_t);
+    int countHardBlocksInArch(t_grid_def& layout ,int hardBlockType,std::pair<int,int> size);
+    void scaleHardBlockCounts();
+
+    void implementUnassignedLogicInSoftLogic(netlist_t* netlist);
+    int inferHardBlocksFromNetlist(int currentOptimizationKind);
+    void chooseHardBlocks(netlist_t *netlist,mix_hard_blocks type);
+    /* This map holds estimated device sizes that would
+     * correspond to the architecture, keyed by the pointers to 
+     * layout  description
+     */
+    std::map<int,std::pair<int,int>> _grid_layout_sizes; 
+
+    // This array is composed of vectors, that store nodes that
+    // are potential candidates for performing mixing optimization
+    std::vector<nnode_t*>candidate_nodes[mix_hard_blocks::Count];
+    int _hardBlocksCount [mix_hard_blocks::Count];
+    // These booleans store devices selected for optimization
+    bool _allOptsDisabled ;
+    bool _enabledOptimizations[mix_hard_blocks::Count];
+  	/* If  mixing soft and hard_logic is set, the possible size of the device 
+	 * should be estimated 
+	 */  
+    OdinGridAnalyzer _analyzer;
+    float _hardBlocksMixingRatio[mix_hard_blocks::Count];
+    t_arch& _arch;
+    std::vector<t_physical_tile_type> _tileTypes;
+};
+
+#endif
---
+++ ODIN_II/SRC/include/OdinGridAnalyzer.hpp
@@ -0,0 +1,63 @@
+/*
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+#ifndef ODIN_GRID_ANALYZER_HPP
+#define ODIN_GRID_ANALYZER_HPP
+
+#include "physical_types.h"
+#include "odin_types.h"
+#include <utility>      // std::pair, std::make_pair
+#include <tuple>
+#include <vector>       // std::vector
+
+class OdinGridAnalyzer{
+    public:
+        /*----------------------------------------------------------------------
+        * Function: estimatePossibleDeviceSize 
+        *   Estimates possible size of the device: for a fixed layout, it is 
+        *   known, for a dynamic layout, some assumptions are required 
+        * Parameters: 
+        *   layout: const t_grid_def : layout to explore
+        * returns:
+        *   Pair of integers, that correspond to estimated width and height of 
+        *   the device
+        *---------------------------------------------------------------------*/
+        std::pair<int,int> estimatePossibleDeviceSize(t_grid_def&);
+
+        int countHardBlocksInFixedLayout(t_grid_def& layout ,int hardBlockType,std::pair<int,int> size,std::vector<t_physical_tile_type>tiletypes);
+        int findDesiredBlockPriority(t_grid_def& layout,std::string& typeTag);
+        ssize_t findIndexOfAssociatedTileType(std::vector<t_physical_tile_type> tileTypes,std::string& typeTag);
+        
+    private:
+        std::string getArchDescriptionTag(int hardBlockType);
+        // int parseExpression(std::string& expr, int position,int sign, int W, int H, int w, int h);
+
+        /**
+         * This function mimics the behaviour of the build_device_grid 
+         * It is segreged into a separate function, to improve maintainability 
+         */
+        void fillGridWithBlockType(std::vector<std::vector<std::pair<char*,int>>>&grid, t_grid_loc_def* grid_def,
+                                     std::vector<t_physical_tile_type>tileTypes, ssize_t indexOfTile,int grid_width,int grid_height);
+
+        void set_grid_block_type(int priority,t_physical_tile_type* type, int x, int y,std::vector<std::vector<std::pair<char*,int>>>&  grid);
+};
+#endif
